4de8f3f8ed0a46556921d718e48d35a04630e996
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2020-12-11 11:15:38 -0700

    first question of ch1 answered

--- c/ch1/expx.c
+++ c/ch1/expx.c
@@ -1,29 +1,42 @@
 #include <petsc.h>
 
 int main(int argc, char **argv) {
-  PetscErrorCode ierr;
-  PetscMPIInt    rank;
-  PetscInt       i;
-  PetscReal      localval, globalsum;
+    
+    PetscErrorCode ierr;
+    PetscMPIInt    rank;
+    PetscInt       i;
+    PetscReal      localval, globalsum;
+    PetscReal      x = 1; // default value
+  
+    ierr = PetscInitialize(&argc, &argv, NULL,
+                           "Compute e in parallel with PETSc.\n\n"); 
+    if (ierr) return ierr;
+  
+    ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank); CHKERRQ(ierr);
 
-  ierr = PetscInitialize(&argc,&argv,NULL,
-      "Compute e in parallel with PETSc.\n\n"); if (ierr) return ierr;
-  ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank); CHKERRQ(ierr);
+    ierr = PetscOptionsBegin(PETSC_COMM_WORLD, NULL, 
+                             "Maclaurin Series for exp(x)",
+                             NULL); CHKERRQ(ierr); 
+      ierr = PetscOptionsReal("-x", "input to exp(x) function", 
+                              NULL, x, &x, NULL); CHKERRQ(ierr);
+    ierr = PetscOptionsEnd(); CHKERRQ(ierr);
 
-  // compute  1/n!  where n = (rank of process) + 1
-  localval = 1.0;
-  for (i = 2; i < rank+1; i++)
-      localval /= i;
+    // compute  x^n / n!  where n = (rank of process) + 1
+    localval = 1.;
+    for (i = 1; i < rank+1; i++) localval *= x/i;
+  
+    // sum the contributions over all processes
+    ierr = MPI_Allreduce(&localval, &globalsum, 1, MPIU_REAL, MPIU_SUM,
+                         PETSC_COMM_WORLD); CHKERRQ(ierr);
+  
+    // output estimate of e and report on work from each process
+    ierr = PetscPrintf(PETSC_COMM_WORLD,
+                       "exp(x) is about %17.15f\n", 
+                       globalsum); CHKERRQ(ierr);
 
-  // sum the contributions over all processes
-  ierr = MPI_Allreduce(&localval,&globalsum,1,MPIU_REAL,MPIU_SUM,
-      PETSC_COMM_WORLD); CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_SELF,
+                       "rank %d did %d flops\n", 
+                       rank, (rank > 0) ? rank-1 : 0); CHKERRQ(ierr);
 
-  // output estimate of e and report on work from each process
-  ierr = PetscPrintf(PETSC_COMM_WORLD,
-      "e is about %17.15f\n",globalsum); CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_SELF,
-      "rank %d did %d flops\n",rank,(rank > 0) ? rank-1 : 0);
-      CHKERRQ(ierr);
-  return PetscFinalize();
+    return PetscFinalize();
 }
